dd9693c8aa2471c8b0969ee855c3d1b9d6a9d31d
dayam <dayam@Xs-MacBook-Air.local>
2020-10-10 17:05:38 -0600

    Corrected eigenvalue file name

--- run_genesis_pca.sh
+++ run_genesis_pca.sh
@@ -6,7 +6,7 @@ plink_bim_file=`echo $plink_bed_file | sed 's/.bed/.bim/' | sed 's/.BED/.BIM/'`
 plink_fam_file=`echo $plink_bed_file | sed 's/.bed/.fam/' | sed 's/.BED/.FAM/'`
 gds_file=`echo $plink_bed_file | sed 's/.bed/.gds/' | sed 's/.BED/.gds/'`
 pcvec_file=`echo $plink_bed_file | sed 's/.bed/.pcvec/' | sed 's/.BED/.pcvec/'`
-pcvec_file=`echo $plink_bed_file | sed 's/.bed/.eigenval/' | sed 's/.BED/.eigenval/'`
+eigenval_file=`echo $plink_bed_file | sed 's/.bed/.eigenval/' | sed 's/.BED/.eigenval/'`
 slide_max_bp=$2
 r2_ld_threshold=$3
 
@@ -18,7 +18,7 @@ cat /home/analyst/GENESIS_PCA.R | R --vanilla --args \
    $plink_fam_file \
    $gds_file \
    $pcvec_file \
-   $pcvec_file \
+   $eigenval_file \
    $slide_max_bp \
    $r2_ld_threshold
 
