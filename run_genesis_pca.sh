0bfa27830b99acf6764d12445278286ba837ed7c
dayam <dayam@Xs-MacBook-Air.local>
2020-10-12 12:58:50 -0600

    Changed run_genesis script to not use the full directory path to infer the output file name, but rather the current working directory (otherwise file write permissions of the output files is an issue

--- run_genesis_pca.sh
+++ run_genesis_pca.sh
@@ -5,13 +5,12 @@ plink_bed_file=$1
 plink_bim_file=`echo $plink_bed_file | sed 's/.bed/.bim/' | sed 's/.BED/.BIM/'`
 plink_fam_file=`echo $plink_bed_file | sed 's/.bed/.fam/' | sed 's/.BED/.FAM/'`
 gds_file=`echo $plink_bed_file | sed 's/.bed/.gds/' | sed 's/.BED/.gds/'`
-rdata_file=`echo $plink_bed_file | sed 's/.bed/.RData/' | sed 's/.BED/.RData/'`
-log_file=`echo $plink_bed_file | sed 's/.bed/_GENESIS_stdout.log/' | sed 's/.BED/_GENESIS_stdout.log/'`
+rdata_file=`basename $plink_bed_file | sed 's/.bed/.RData/' | sed 's/.BED/.RData/'`
+log_file=`basename $plink_bed_file | sed 's/.bed/_GENESIS_stdout.log/' | sed 's/.BED/_GENESIS_stdout.log/'`
 slide_max_bp=$2
 r2_ld_threshold=$3
 
 #Call R script
-
 cat /home/analyst/GENESIS_PCA.R | R --vanilla --args \
    $plink_bed_file \
    $plink_bim_file \
